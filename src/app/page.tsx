cf21521561d1c566723e16c151f2c5c67778586c
bbadrakh <bbadrakhofficial@gmail.com>
2024-11-21 21:00:50 +0800

    coutdown test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  return <div></div>;
+  const [count, setCount] = useState<number>(0);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setCount(count + 1);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, [count]);
+  return (
+    <div>
+      <p>Hello world</p>
+      <p>{count}</p>
+    </div>
+  );
 }
